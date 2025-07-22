5b8eab4dd43c7e2b0df0d28503d4ce6d4e944f65
dewangupadhyay4 <dewangupadhyay23@gmail.com>
2025-07-22 20:37:41 +0530

    this is new commit

--- finnone/src/main/java/com/finnone/config/ModelMapperConfig.java
+++ finnone/src/main/java/com/finnone/config/ModelMapperConfig.java
@@ -1,15 +1,15 @@
-package com.finnone.config;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ModelMapperConfig {
-	
-	@Bean
-	public ModelMapper modelMapper(){
-		return new ModelMapper();
-	}
-
-}
+//package com.finnone.config;
+//
+//import org.modelmapper.ModelMapper;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ModelMapperConfig {
+//	
+//	@Bean
+//	public ModelMapper modelMapper(){
+//		return new ModelMapper();
+//	}
+//
+//}
